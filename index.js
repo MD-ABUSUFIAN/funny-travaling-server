c0dfe302da49d8a1fe899052c3f7a8b9893ec14f
MD-ABUSUFIAN <abusufian.programmer@gmail.com>
2021-10-30 23:33:01 +0600

    post method successfully

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ const port=process.env.PORT ||5000;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0epdq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+
 app.use(cors());
-app.use(express());
+app.use(express.json());
 
 
 
@@ -28,13 +29,27 @@ try{
     //   const result = await serviceCollection.insertOne(doc);
     //   console.log(result);
 
+    app.post('/addnew',async(req,res)=>{
+      
+        const result = await serviceCollection.insertOne(req.body);
+        res.send(result);
+        console.log("hit the post api client site",req.body);
 
-      app.get('/services',async(req,res)=>{
 
+    });
+
+        app.get('/services',async(req,res)=>{
+        
         const result = await serviceCollection.find({}).toArray();
         res.send(result);
-        console.log(result);
-      })
+      
+      });
+
+
+
+     
+
+
 
 }
 
